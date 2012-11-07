f9b5b66d135b028cbee4b8fe790327a0ee4732c2
Erick Matsen <matsen@fhcrc.org>
2012-11-07 12:40:28 -0800

    gsl_multifit_fdfsolver_lmsder werkin!

--- test.c
+++ test.c
@@ -12,9 +12,7 @@ struct data {
   double * l;
 };
 
-int
-expb_f (const gsl_vector * x, void *params,
-        gsl_vector * f)
+int expb_f (const gsl_vector * x, void *params, gsl_vector * f)
 {
   size_t n = ((struct data *)params)->n;
   double *t = ((struct data *)params)->t;
@@ -25,50 +23,45 @@ expb_f (const gsl_vector * x, void *params,
   double r = gsl_vector_get (x, 2);
 
   size_t i;
+  double ert;
 
   for (i = 0; i < n; i++)
     {
-      /* Model Yi = A * exp(-lambda * i) + b */
-      double t = i;
-      double Yi = A * exp (-lambda * t) + b;
-      gsl_vector_set (f, i, (Yi - y[i])/sigma[i]);
+      /* Model l[i] = c*log((1+exp(-r*t[i]))/2)+m*log((1-exp(-r*t[i]))/2) */
+      ert = exp(-r*t[i]);
+      gsl_vector_set (f, i, c*log((1+ert)/2)+m*log((1-ert)/2) - l[i]);
     }
 
   return GSL_SUCCESS;
 }
 
-int
-expb_df (const gsl_vector * x, void *params,
-         gsl_matrix * J)
+int expb_df (const gsl_vector * x, void *params, gsl_matrix * J)
 {
   size_t n = ((struct data *)params)->n;
-  double *sigma = ((struct data *) params)->sigma;
+  double *t = ((struct data *)params)->t;
+  double *l = ((struct data *) params)->l;
 
-  double A = gsl_vector_get (x, 0);
-  double lambda = gsl_vector_get (x, 1);
+  double c = gsl_vector_get (x, 0);
+  double m = gsl_vector_get (x, 1);
+  double r = gsl_vector_get (x, 2);
 
   size_t i;
+  double ert;
 
-  for (i = 0; i < n; i++)
-    {
-      /* Jacobian matrix J(i,j) = dfi / dxj, */
-      /* where fi = (Yi - yi)/sigma[i],      */
-      /*       Yi = A * exp(-lambda * i) + b  */
-      /* and the xj are the parameters (A,lambda,b) */
-      double t = i;
-      double s = sigma[i];
-      double e = exp(-lambda * t);
-      gsl_matrix_set (J, i, 0, e/s);
-      gsl_matrix_set (J, i, 1, -t * A * e/s);
-      gsl_matrix_set (J, i, 2, 1/s);
+  for (i = 0; i < n; i++) {
+      /* nx3 Jacobian matrix J(i,j) = dfi / dxj, */
+      /* where fi = = c*log((1+exp(-r*t[i]))/2)+m*log((1-exp(-r*t[i]))/2) - l[i] */
+      /* and the xj are the parameters (c, m, r) */
 
-    }
+	ert = exp(-r*t[i]);
+      gsl_matrix_set (J, i, 0, log((1+ert)/2)); /* df/dc */
+      gsl_matrix_set (J, i, 1, log((1-ert)/2)); /* df/dm */
+      gsl_matrix_set (J, i, 2, t[i]*(-c*ert/(1+ert)+m*ert/(1-ert))); /* df/dt */
+  }
   return GSL_SUCCESS;
 }
 
-int
-expb_fdf (const gsl_vector * x, void *params,
-          gsl_vector * f, gsl_matrix * J)
+int expb_fdf (const gsl_vector * x, void *params, gsl_vector * f, gsl_matrix * J)
 {
   expb_f (x, params, f);
   expb_df (x, params, J);
@@ -76,60 +69,51 @@ expb_fdf (const gsl_vector * x, void *params,
   return GSL_SUCCESS;
 }
 
-//The main part of the program sets up a Levenberg-Marquardt solver and some simulated random data. The data uses the known parameters (1.0,5.0,0.1) combined with gaussian noise (standard deviation = 0.1) over a range of 40 timesteps. The initial guess for the parameters is chosen as (0.0, 1.0, 0.0).
+int print_state (unsigned int iter, gsl_multifit_fdfsolver * s)
+{
+  printf ("iter: %3u x = % 15.8f % 15.8f % 15.8f "
+          "|f(x)| = %g\n",
+          iter,
+          gsl_vector_get (s->x, 0),
+          gsl_vector_get (s->x, 1),
+          gsl_vector_get (s->x, 2),
+          gsl_blas_dnrm2 (s->f));
+}
 
-int
-main (void)
+int main (void)
 {
   const gsl_multifit_fdfsolver_type *T;
   gsl_multifit_fdfsolver *s;
 
   int status;
-  size_t i, iter = 0;
+  size_t i;
+  unsigned int iter = 0;
 
-  const size_t n = 40;
-  const size_t p = 3;
+  const size_t n = 3;
 
-  gsl_matrix *covar = gsl_matrix_alloc (p, p);
+  gsl_matrix *covar = gsl_matrix_alloc (3, 3);
 
-  double y[n], sigma[n];
+  double t[3]={0.25,0.5,1}, l[3] = {-2.787877533411486, -2.721250292026327, -3.05124979115564};
 
-  struct data d = { n, y, sigma};
+  struct data d = {n, t, l};
 
   gsl_multifit_function_fdf f;
 
-  double x_init[3] = { 1.0, 0.0, 0.0 };
+  double x_init[3] = { 3.0, 4.0, 5.0 };
 
-  gsl_vector_view x = gsl_vector_view_array (x_init, p);
-
-  const gsl_rng_type * type;
-  gsl_rng * r;
-
-  gsl_rng_env_setup();
-
-  type = gsl_rng_default;
-  r = gsl_rng_alloc (type);
+  gsl_vector_view x = gsl_vector_view_array (x_init, 3);
 
   f.f = &expb_f;
   f.df = &expb_df;
   f.fdf = &expb_fdf;
   f.n = n;
-  f.p = p;
+  f.p = 3;
   f.params = &d;
 
   /* This is the data to be fitted */
 
-  for (i = 0; i < n; i++)
-    {
-      double t = i;
-      y[i] = 1.0 + 5 * exp (-0.1 * t)
-                 + gsl_ran_gaussian(r, 0.1);
-      sigma[i] = 0.1;
-      printf("data: %d %g %g\n", i, y[i], sigma[i]);
-    };
-
   T = gsl_multifit_fdfsolver_lmsder;
-  s = gsl_multifit_fdfsolver_alloc (T, n, p);
+  s = gsl_multifit_fdfsolver_alloc (T, n, 3);
   gsl_multifit_fdfsolver_set (s, &f, &x.vector);
 
   print_state (iter, s);
@@ -158,9 +142,9 @@ main (void)
 #define FIT(i) gsl_vector_get(s->x, i)
 #define ERR(i) sqrt(gsl_matrix_get(covar,i,i))
 
-  printf("A      = %.5f +/- %.5f\n", FIT(0), ERR(0));
-  printf("lambda = %.5f +/- %.5f\n", FIT(1), ERR(1));
-  printf("b      = %.5f +/- %.5f\n", FIT(2), ERR(2));
+  printf("c = %.5f +/- %.5f\n", FIT(0), ERR(0));
+  printf("m = %.5f +/- %.5f\n", FIT(1), ERR(1));
+  printf("r = %.5f +/- %.5f\n", FIT(2), ERR(2));
 
   printf ("status = %s\n", gsl_strerror (status));
 
@@ -168,32 +152,3 @@ main (void)
   return 0;
 }
 
-int
-print_state (size_t iter, gsl_multifit_fdfsolver * s)
-{
-  printf ("iter: %3u x = % 15.8f % 15.8f % 15.8f "
-          "|f(x)| = %g\n",
-          iter,
-          gsl_vector_get (s->x, 0),
-          gsl_vector_get (s->x, 1),
-          gsl_vector_get (s->x, 2),
-          gsl_blas_dnrm2 (s->f));
-}
-
-//The iteration terminates when the change in x is smaller than 0.0001, as both an absolute and relative change. Here are the results of running the program,
-//
-//iter: 0 x = 1.00000000 0.00000000 0.00000000 |f(x)| = 118.574
-//iter: 1 x = 1.64919392 0.01780040 0.64919392 |f(x)| = 77.2068
-//iter: 2 x = 2.86269020 0.08032198 1.45913464 |f(x)| = 38.0579
-//iter: 3 x = 4.97908864 0.11510525 1.06649948 |f(x)| = 10.1548
-//iter: 4 x = 5.03295496 0.09912462 1.00939075 |f(x)| = 6.4982
-//iter: 5 x = 5.05811477 0.10055914 0.99819876 |f(x)| = 6.33121
-//iter: 6 x = 5.05827645 0.10051697 0.99756444 |f(x)| = 6.33119
-//iter: 7 x = 5.05828006 0.10051819 0.99757710 |f(x)| = 6.33119
-//
-//A      = 5.05828 +/- 0.05983
-//lambda = 0.10052 +/- 0.00309
-//b      = 0.99758 +/- 0.03944
-//status = success
-//
-//The approximate values of the parameters are found correctly. The errors on the parameters are given by the square roots of the diagonal elements of the covariance matrix.
