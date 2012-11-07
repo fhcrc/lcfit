8ebd933553babf75e62274be7c892537c4258198
Erick Matsen <matsen@fhcrc.org>
2012-11-07 12:42:25 -0800

    cleaning out quintic stuff

--- quintic_C.h
+++
@@ -1,7 +0,0 @@
-#include <math.h>
-
-int quintic(double [], double [], double [], int*, double);
-int quartic(double[], double[], double[], int* );
-int cubic(double[], double[], int*);
-int signR(double);
-double CBRT(double);
