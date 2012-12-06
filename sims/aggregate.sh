4042444312433747aa0a3f9293d20bc3518876b9
Connor McCoy <cmccoy@fhcrc.org>
2012-12-06 11:55:00 -0800

    No longer varying sample points

--- sims/aggregate.sh
+++ sims/aggregate.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-nestagg delim -d runs lcfit_plots.txt -t -o lcfit_plots_agg.txt -k initial
+nestagg delim -d runs lcfit_plots.txt -t -o lcfit_plots_agg.txt -k initial,ml_tree
 ./aggregate.R
