4024ad9bee957113a66062bb2761d24a3214b73f
Connor McCoy <cmccoy@fhcrc.org>
2012-12-06 12:49:16 -0800

    Add JTT92, WAG01, HKY85-2.0

--- sims/aggregate.R
+++ sims/aggregate.R
@@ -2,7 +2,7 @@
 
 library(ggplot2)
 agg <- read.delim('lcfit_plots_agg.txt', sep='\t')
-p <- ggplot(agg, aes(x=t, y=rss, shape=initial, color=initial)) +
+p <- ggplot(agg, aes(x=t, y=rss, shape=model_name, color=model_name)) +
   geom_point(size=1.2) +
   ggtitle("RSS by branch length") +
   xlab("ML Branch length") +
