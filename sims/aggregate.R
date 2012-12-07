08592ddaaaf434c4fe8634dfc6bcc260f1d3c961
Connor McCoy <cmccoy@fhcrc.org>
2012-12-06 17:16:24 -0800

    Add another plot
    ML branch length obtained via actual ML versus lcfit

--- sims/aggregate.R
+++ sims/aggregate.R
@@ -10,3 +10,15 @@ p <- ggplot(agg, aes(x=t, y=rss, shape=model_name, color=model_name)) +
   theme_bw() +
   xlim(0,2)
 ggsave('rss_bl.svg', width=7,height=5)
+
+maxima <- read.csv('lcfit_maxima.csv', as.is=TRUE)
+p <- ggplot(maxima, aes(x=t, y=t_hat, color=model_name)) +
+    geom_point(size=1.2) +
+    geom_smooth(method='lm', se=FALSE) +
+    geom_abline(slope=1, intercept=0, linetype='dashed', color='grey') +
+    labs(x='t', y=expression(hat(t))) +
+    ggtitle("lcfit versus ML branch length") +
+    facet_wrap(~model_name) +
+    theme(legend.position='none')
+ggsave('ml_lcfit_comparison.svg')
+
--- sims/aggregate.sh
+++ sims/aggregate.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 nestagg delim -d runs lcfit_plots.txt -t -o lcfit_plots_agg.txt -k initial,ml_tree,model_name
+nestagg delim -d runs lcfit_maxima.csv -o lcfit_maxima.csv -k ml_tree,model_name
 ./aggregate.R
