6a7f1320fb946fa8c53e0a3d56db018d17473438
Connor McCoy <cmccoy@fhcrc.org>
2012-12-12 13:18:25 -0800

    Ignore parameters - only optimize branch lengths.

--- sims/aggregate.sh
+++ sims/aggregate.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+set -e
+set -u
 
 #nestagg delim -d runs lcfit_plots.txt -t -o lcfit_plots_agg.txt -k initial,ml_tree,model_name
-nestagg delim -d runs lcfit_maxima.csv -o lcfit_maxima.csv -k ml_tree,model_name
+nestagg delim -d runs lcfit_maxima.csv -o lcfit_maxima.csv -k ml_tree,model_name -m warn
 ./aggregate.R
