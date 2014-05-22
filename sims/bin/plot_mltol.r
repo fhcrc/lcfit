430ae73bc5f50198c0e5112b98b2c3a5ffc280b6
Connor McCoy <cmccoy@fhcrc.org>
2014-05-22 15:13:14 -0700

    ML tolerance

--- sims/bin/plot_mltol.r
+++ sims/bin/plot_mltol.r
@@ -8,19 +8,19 @@ theme_set(theme_bw(16) + theme(strip.background = element_blank()))
 agg <- read.csv('agg_mltol.csv', as.is = TRUE)
 
 to_plot <- transform(agg, tol = factor(tolerance),
-                     n_eval_diff = n_eval - n_eval_brent)
+                     n_eval_ratio = n_eval / n_eval_brent)
 
 x_lev <- as.character(to_plot$tolerance)
 x_lab <- sprintf('10^%d', log10(to_plot$tolerance))
 x_lab <- parse(text = x_lab)
 
 
-p1 <- ggplot(to_plot, aes(x = tol, y = n_eval_diff)) +
+p1 <- ggplot(to_plot, aes(x = tol, y = n_eval_ratio)) +
   geom_boxplot() +
   xlab("Tolerance") +
-  geom_hline(yintercept = 0.0, linetype = 'dashed') +
   scale_x_discrete(breaks = x_lev, labels = x_lab) +
-  ylab("Branch log-likelihood calls: lcfit - Brent")
+  ylab("Branch log-likelihood calls: lcfit / Brent") +
+  ylim(0, 1)
 ggsave('branch_likelihood_calls_by_tol.svg', width = 8, height = 5)
 
 p2 <- ggplot(agg, aes(x = ml_brent, y = ml_est)) +
