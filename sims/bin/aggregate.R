0d39dcd635bc8ffc55f805422b86598bff1b73a8
Connor McCoy <cmccoy@fhcrc.org>
2014-05-28 10:01:14 -0700

    more figs

--- sims/bin/aggregate.R
+++ sims/bin/aggregate.R
@@ -3,6 +3,7 @@
 library(ggplot2)
 library(gridExtra)
 library(plyr)
+library(reshape2)
 library(RJSONIO)
 suppressPackageStartupMessages(library(distrEx))
 suppressPackageStartupMessages(library(entropy))
@@ -53,11 +54,19 @@ error <- ldply(controls, function(p) {
   bls_file <- p$lcfit[1]
   maxima_file <- p$lcfit[2]
   fit_file <- p$lcfit[3]
+  ml_est_file <- p$lcfit[4]
 
   bls <- read.csv(bls_file, as.is=TRUE)
   maxima <- read.csv(maxima_file, as.is=TRUE)
+  ml_est <- read.csv(ml_est_file, as.is=TRUE)
+
+  # Choosing one tolerance for now
+  ml_est <- ml_est[ml_est[['tolerance']] == 1e-3, c('node_id', 'ml_est', 'ml_brent')]
+  colnames(ml_est)[2:3] <- c('t_hat_iterative', 't_hat_brent')
+
   error <- ddply(bls, .(node_id), function(d) compare_bls(d))
   error <- join(error, maxima, by=c('node_id'), type='left', match='first')
+  error <- join(error, ml_est, by=c('node_id'), type='left', match='first')
   transform(error,
             model=p$model_name,
             rate=p$rdist_name,
@@ -66,7 +75,10 @@ error <- ldply(controls, function(p) {
             seed=p$seed,
             tree=p$source_tree,
             branch_length_rate=p$branch_length_rate,
+            mean_branch_length=sprintf('%.2f', 1 / p$branch_length_rate),
             mean_branch_distribution=sprintf('Exp(mu == %.2f)', 1 / p$branch_length_rate),
+            iterative_rel_err=abs(t - t_hat_iterative) / t,
+            iterative_abs_err=abs(t - t_hat_iterative),
             rel_err=abs(t - t_hat) / t,
             abs_err=abs(t - t_hat))
 }, .progress = 'text')
@@ -74,7 +86,7 @@ error <- ldply(controls, function(p) {
 
 error$model <- factor(error$model, levels = c('Binary-0.25', 'Binary-1.0', 'Binary-4.0',
                                               'JC', 'HKY85-2.0',
-                                              'JTT92', 'LG08'))
+                                              'JTT92', 'LG08', 'YN98-2-5'))
 error$mean_branch_distribution <- factor(error$mean_branch_distribution,
                                          levels = sort(unique(as.character(error$mean_branch_distribution))))
 
@@ -124,12 +136,20 @@ local({
     scale_color_discrete(name = 'Branch length rate') +
     theme(legend.position = 'bottom') +
     ggtitle("Comparison of fit branch lengths")
+  p3 <- ggplot(error, aes(x=t, y=abs_err, color=factor(mean_branch_distribution))) +
+    facet_grid(rate ~ model) +
+    geom_point() +
+    ylab(expression(abs(hat(t) - t))) +
+    xlab('ML branch length') +
+    scale_color_discrete(name = 'Branch length rate') +
+    theme(legend.position = 'bottom') +
+    ggtitle("Absolute error")
 
-  svg('ml_branch_length_vs_rel_err.svg', width=14, height=12)
-  tryCatch(grid.arrange(p1, p2, nrow=2), finally=dev.off())
+  svg('ml_branch_length_vs_rel_err.svg', width=14, height=18)
+  tryCatch(grid.arrange(p1, p2, p3, nrow=3), finally=dev.off())
 })
 
-p5 <- ggplot(error, aes(x=t, y=t_hat, color=factor(mean_branch_distribution))) +
+p5 <- ggplot(error, aes(x=t, y=t_hat, color=factor(mean_branch_length))) +
   facet_grid(rate ~ model) +
   geom_point() +
   geom_abline(slope=1) +
@@ -139,6 +159,16 @@ p5 <- ggplot(error, aes(x=t, y=t_hat, color=factor(mean_branch_distribution))) +
   xlab('ML branch length') +
   theme(legend.position = 'bottom')
 ggsave('t_vs_that.svg', p5, width=14, height=9, dpi=72)
+
+m <- melt(error, measure.vars = c('t_hat', 't_hat_iterative'))
+m[['variable']] <- factor(m[['variable']], levels = c('t_hat', 't_hat_iterative'),
+                          labels = c('non-iterative', 'iterative'))
+p6 <- ggplot(m, aes(x = variable, y = t - value)) +
+  geom_hline(yintercept = 0, linetype = 'dashed') +
+  geom_boxplot() +
+  xlab('Fit method') +
+  ylab(expression(t - hat(t)))
+ggsave('t_vs_that_iter.svg', p6, dpi = 72, width = 5, height = 5)
 
 ggsave('hellinger.png', p1, height=14, width=7, dpi=72)
 ggsave('kl.svg', p2, height=6, width=10, dpi=72)
--- sims/bin/plot_mltol.r
+++ sims/bin/plot_mltol.r
@@ -8,12 +8,14 @@ theme_set(theme_bw(16) + theme(strip.background = element_blank()))
 agg <- read.csv('agg_mltol.csv', as.is = TRUE)
 
 to_plot <- transform(agg, tol = factor(tolerance),
+                     tol_str = sprintf('10^%d', log10(tolerance)),
+                     rel_err = abs(ml_brent - ml_est) / ml_brent,
+                     abs_err = abs(ml_brent - ml_est),
                      n_eval_ratio = n_eval / n_eval_brent)
 
 x_lev <- as.character(to_plot$tolerance)
 x_lab <- sprintf('10^%d', log10(to_plot$tolerance))
 x_lab <- parse(text = x_lab)
-
 
 p1 <- ggplot(to_plot, aes(x = tol, y = n_eval_ratio)) +
   geom_boxplot() +
@@ -23,9 +25,12 @@ p1 <- ggplot(to_plot, aes(x = tol, y = n_eval_ratio)) +
   ylim(0, 1)
 ggsave('branch_likelihood_calls_by_tol.svg', width = 8, height = 5)
 
-p2 <- ggplot(agg, aes(x = ml_brent, y = ml_est)) +
-  geom_abline(slope = 1) +
+p2 <- ggplot(to_plot, aes(x = ml_brent, y = ml_est)) +
+  geom_abline(slope = 1, color = 'grey', linetype = 'dashed') +
   geom_point() +
   coord_equal() +
-  facet_wrap(~tolerance)
-p2
+  xlab("ML branch length (Brent)") +
+  ylab("ML branch length (lcfit)") +
+  facet_grid(~ tol_str, labeller = label_parsed)
+ggsave('ml_err_by_tol.svg', width = 5, height = 4, dpi = 72)
+
