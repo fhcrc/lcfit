a372bcda2403fccccd879fe26581d4428c5526b3
Brian Claywell <bclaywel@fhcrc.org>
2016-03-08 17:30:30 -0800

    Initial commit of constraints.

--- lcfit_src/lcfit2_nlopt.c
+++ lcfit_src/lcfit2_nlopt.c
@@ -66,6 +66,47 @@ double lcfit2_opt_fdf_nlopt(unsigned p, const double* x, double* grad, void* dat
     return sum_sq_err;
 }
 
+//
+// nlopt expects constraints of the form fc(x) <= 0
+//
+
+// constraint: c > m
+// becomes m - c < 0
+
+double lcfit2_cons_cm_nlopt(unsigned p, const double* x, double* grad, void* data)
+{
+    const double c = x[0];
+    const double m = x[1];
+
+    if (grad) {
+        grad[0] = -1.0;
+        grad[1] = 1.0;
+    }
+
+    return m - c;
+}
+
+// constraint: c + m - v > 0 which implies t0 <= (1/r)*log((c+m)/(c-m))
+// becomes t0 - (1/r)*log((c+m)/(c-m)) <= 0
+double lcfit2_cons_cmv_nlopt(unsigned p, const double* x, double* grad, void* data)
+{
+    lcfit2_fit_data* d = (lcfit2_fit_data*) data;
+
+    const double c = x[0];
+    const double m = x[1];
+    const double t_0 = d->t0;
+    const double f_2 = d->d2;
+
+    if (grad) {
+        grad[0] = (1.0L/2.0L)*pow(c - m, 2)*(-1/(c - m) + (c + m)/pow(c - m, 2))/(sqrt(-c*f_2*m/(c + m))*(c + m)) - 1.0L/2.0L*log((c + m)/(c - m))/sqrt(-c*f_2*m/(c + m)) - 1.0L/4.0L*(c - m)*(-c*f_2*m/pow(c + m, 2) + f_2*m/(c + m))*log((c + m)/(c - m))/pow(-c*f_2*m/(c + m), 3.0L/2.0L);
+
+        grad[1] = -1.0L/2.0L*pow(c - m, 2)*(1.0/(c - m) + (c + m)/pow(c - m, 2))/(sqrt(-c*f_2*m/(c + m))*(c + m)) + (1.0L/2.0L)*log((c + m)/(c - m))/sqrt(-c*f_2*m/(c + m)) - 1.0L/4.0L*(c - m)*(-c*f_2*m/pow(c + m, 2) + c*f_2/(c + m))*log((c + m)/(c - m))/pow(-c*f_2*m/(c + m), 3.0L/2.0L);
+    }
+
+    return t_0 - 1.0L/2.0L*(c - m)*log((c + m)/(c - m))/sqrt(-c*f_2*m/(c + m));
+}
+
+
 int lcfit2_fit_weighted_nlopt(const size_t n, const double* t, const double* lnl,
                               const double* w, lcfit2_bsm_t* model)
 {
@@ -78,6 +119,9 @@ int lcfit2_fit_weighted_nlopt(const size_t n, const double* t, const double* lnl
     nlopt_set_min_objective(opt, lcfit2_opt_fdf_nlopt, &data);
     nlopt_set_lower_bounds(opt, lower_bounds);
     nlopt_set_upper_bounds(opt, upper_bounds);
+
+    nlopt_add_inequality_constraint(opt, lcfit2_cons_cm_nlopt, &data, 0.0);
+    nlopt_add_inequality_constraint(opt, lcfit2_cons_cmv_nlopt, &data, 0.0);
 
     nlopt_set_xtol_rel(opt, sqrt(DBL_EPSILON));
     nlopt_set_maxeval(opt, MAX_ITERATIONS);
