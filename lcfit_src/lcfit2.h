2cab0b122dbda1a2b7cd14495be253ce0b498101
Brian Claywell <bclaywel@fhcrc.org>
2016-04-21 18:04:40 -0700

    Update comments.

--- lcfit_src/lcfit2.h
+++ lcfit_src/lcfit2.h
@@ -49,7 +49,7 @@ double lcfit2_infl_t(const lcfit2_bsm_t* model);
 /** Computes the gradient of the normalized log-likelihood function at branch length \c t for a given model. */
 void lcfit2n_gradient(const double t, const lcfit2_bsm_t* model, double* grad);
 
-/** Computes the unnormalized log-likelihood at branch length \c t for a given model. */
+/** Computes the log-likelihood at branch length \c t for a given model. */
 double lcfit2_lnl(const double t, const lcfit2_bsm_t* model);
 
 /** Computes the normalized log-likelihood at branch length \c t for a given model. */
@@ -63,7 +63,7 @@ int lcfit2n_fit(const size_t n, const double* t, const double* lnl,
 int lcfit2n_fit_weighted(const size_t n, const double* t, const double* lnl,
                          const double* w, lcfit2_bsm_t* model);
 
-/** Fits a model to a normalized log-likelihood function. */
+/** Fits a model to a log-likelihood function. */
 int lcfit2_fit_auto(double (*lnl_fn)(double, void*), void* lnl_fn_args,
                     lcfit2_bsm_t* model, const double min_t, const double max_t,
                     const double alpha);
