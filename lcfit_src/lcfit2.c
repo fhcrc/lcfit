c0acde619d7b13e6f83b8d49e497fff2c853d453
Brian Claywell <bclaywel@fhcrc.org>
2017-01-27 11:13:36 -0800

    Remove assert.

--- lcfit_src/lcfit2.c
+++ lcfit_src/lcfit2.c
@@ -143,8 +143,6 @@ double lcfit2_infl_t(const lcfit2_bsm_t* model)
 
     const double infl_t = -b + (1.0 / r) * log((c + m + 2 * sqrt(c * m)) / (c - m));
 
-    assert(fabs(lcfit2_d2f_t(infl_t, model)) <= 1e-6);
-
     return infl_t;
 }
 
