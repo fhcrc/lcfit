94391a90c409ea64af1ed13c07357aea80d2cc67
Brian Claywell <bclaywel@fhcrc.org>
2017-03-23 13:55:17 -0700

    [WIP] Batch rename lcfit2 to lcfit3.

--- lcfit_src/lcfit3_nlopt.c
+++ lcfit_src/lcfit3_nlopt.c
@@ -1,9 +1,9 @@
 /**
- * \file lcfit2_nlopt.c
- * \brief Implementation of lcfit2 optimization using NLopt.
+ * \file lcfit3_nlopt.c
+ * \brief Implementation of lcfit3 optimization using NLopt.
  */
 
-#include "lcfit2_nlopt.h"
+#include "lcfit3_nlopt.h"
 
 #include <assert.h>
 #include <float.h>
@@ -12,11 +12,11 @@
 
 #include <nlopt.h>
 
-#include "lcfit2.h"
+#include "lcfit3.h"
 
 static const size_t MAX_ITERATIONS = 1000;
 
-void lcfit2_print_state_nlopt(double sum_sq_err, const double* x, const double* grad)
+void lcfit3_print_state_nlopt(double sum_sq_err, const double* x, const double* grad)
 {
     size_t iter = 0;
 
@@ -42,16 +42,16 @@ void lcfit2_print_state_nlopt(double sum_sq_err, const double* x, const double* 
  *
  * \return Sum of squared error from observed log-likelihoods.
  */
-double lcfit2n_opt_fdf_nlopt(unsigned p, const double* x, double* grad, void* data)
+double lcfit3n_opt_fdf_nlopt(unsigned p, const double* x, double* grad, void* data)
 {
-    lcfit2_fit_data* d = (lcfit2_fit_data*) data;
+    lcfit3_fit_data* d = (lcfit3_fit_data*) data;
 
     const size_t n = d->n;
     const double* t = d->t;
     const double* lnl = d->lnl;
     const double* w = d->w;
 
-    lcfit2_bsm_t model = { x[0], x[1], d->t0, d->d1, d->d2 };
+    lcfit3_bsm_t model = { x[0], x[1], d->t0, d->d1, d->d2 };
 
     double sum_sq_err = 0.0;
 
@@ -67,24 +67,24 @@ double lcfit2n_opt_fdf_nlopt(unsigned p, const double* x, double* grad, void* da
         // We expect that the observed log-likelihoods have already
         // been normalized. The error is therefore the sum of squared
         // differences between those log-likelihoods and the
-        // normalized lcfit2 log-likelihoods f(t[i]) - f(t0).
+        // normalized lcfit3 log-likelihoods f(t[i]) - f(t0).
         //
 
-        const double err = lnl[i] - lcfit2_norm_lnl(t[i], &model);
+        const double err = lnl[i] - lcfit3_norm_lnl(t[i], &model);
 
         sum_sq_err += w[i] * pow(err, 2.0);
 
         if (grad) {
-            lcfit2n_gradient(t[i], &model, grad_i);
+            lcfit3n_gradient(t[i], &model, grad_i);
 
             grad[0] -= 2 * w[i] * err * grad_i[0];
             grad[1] -= 2 * w[i] * err * grad_i[1];
         }
     }
 
-#ifdef LCFIT2_VERBOSE
-    lcfit2_print_state_nlopt(sum_sq_err, x, grad);
-#endif /* LCFIT2_VERBOSE */
+#ifdef LCFIT3_VERBOSE
+    lcfit3_print_state_nlopt(sum_sq_err, x, grad);
+#endif /* LCFIT3_VERBOSE */
 
     return sum_sq_err;
 }
@@ -94,7 +94,7 @@ double lcfit2n_opt_fdf_nlopt(unsigned p, const double* x, double* grad, void* da
  * NLopt expects constraint functions of the form \f$f_c(x) \leq 0\f$,
  * so we use \f$f_c(x) = m - c\f$. That \f$c\f$ must be strictly
  * greater than \f$m\f$ is handled by the SLSQP algorithm itself, as
- * the lcfit2 log-likelihood function will return \c NaN in the case
+ * the lcfit3 log-likelihood function will return \c NaN in the case
  * where \f$c = m\f$.
  *
  * \param[in]  p     Number of model parameters.
@@ -104,7 +104,7 @@ double lcfit2n_opt_fdf_nlopt(unsigned p, const double* x, double* grad, void* da
  *
  * \return Value of the constraint function at \c x.
  */
-double lcfit2_cons_cm_nlopt(unsigned p, const double* x, double* grad, void* data)
+double lcfit3_cons_cm_nlopt(unsigned p, const double* x, double* grad, void* data)
 {
     const double c = x[0];
     const double m = x[1];
@@ -135,9 +135,9 @@ double lcfit2_cons_cm_nlopt(unsigned p, const double* x, double* grad, void* dat
  *
  * \return Value of the constraint function at \c x.
  */
-double lcfit2_cons_cmv_nlopt(unsigned p, const double* x, double* grad, void* data)
+double lcfit3_cons_cmv_nlopt(unsigned p, const double* x, double* grad, void* data)
 {
-    lcfit2_fit_data* d = (lcfit2_fit_data*) data;
+    lcfit3_fit_data* d = (lcfit3_fit_data*) data;
 
     const double c = x[0];
     const double m = x[1];
@@ -153,21 +153,21 @@ double lcfit2_cons_cmv_nlopt(unsigned p, const double* x, double* grad, void* da
     return t_0 - 1.0L/2.0L*(c - m)*log((c + m)/(c - m))/sqrt(-c*f_2*m/(c + m));
 }
 
-int lcfit2n_fit_weighted_nlopt(const size_t n, const double* t, const double* lnl,
-                               const double* w, lcfit2_bsm_t* model)
+int lcfit3n_fit_weighted_nlopt(const size_t n, const double* t, const double* lnl,
+                               const double* w, lcfit3_bsm_t* model)
 {
-    lcfit2_fit_data data = { n, t, lnl, w, model->t0, model->d1, model->d2 };
+    lcfit3_fit_data data = { n, t, lnl, w, model->t0, model->d1, model->d2 };
 
     const double lower_bounds[2] = { 1.0, 1.0 };
     const double upper_bounds[2] = { INFINITY, INFINITY };
 
     nlopt_opt opt = nlopt_create(NLOPT_LD_SLSQP, 2);
-    nlopt_set_min_objective(opt, lcfit2n_opt_fdf_nlopt, &data);
+    nlopt_set_min_objective(opt, lcfit3n_opt_fdf_nlopt, &data);
     nlopt_set_lower_bounds(opt, lower_bounds);
     nlopt_set_upper_bounds(opt, upper_bounds);
 
-    nlopt_add_inequality_constraint(opt, lcfit2_cons_cm_nlopt, &data, 0.0);
-    nlopt_add_inequality_constraint(opt, lcfit2_cons_cmv_nlopt, &data, 0.0);
+    nlopt_add_inequality_constraint(opt, lcfit3_cons_cm_nlopt, &data, 0.0);
+    nlopt_add_inequality_constraint(opt, lcfit3_cons_cmv_nlopt, &data, 0.0);
 
     nlopt_set_xtol_rel(opt, sqrt(DBL_EPSILON));
     nlopt_set_maxeval(opt, MAX_ITERATIONS);
