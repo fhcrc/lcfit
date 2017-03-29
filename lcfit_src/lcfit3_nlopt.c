01a988cc177293ea8b2eeeac3a7cb542b324e9bd
Brian Claywell <bclaywel@fhcrc.org>
2017-03-28 19:13:31 -0700

    [WIP] Implement theta_b upper bound constraint.

--- lcfit_src/lcfit3_nlopt.c
+++ lcfit_src/lcfit3_nlopt.c
@@ -120,6 +120,35 @@ double lcfit3_cons_cm_nlopt(unsigned p, const double* x, double* grad, void* dat
     return m - c;
 }
 
+/** NLopt constraint function and its gradient for enforcing that \f$\theta_b < (c + m)/(c - m)\f$.
+ *
+ * NLopt expects constraint functions of the form \f$f_c(x) \leq 0\f$, so we use
+ * \f[
+ *   f_c(x) = \theta_b - \frac{c + m}{c - m}
+ * \f]
+ *
+ * \param[in]  p     Number of model parameters.
+ * \param[in]  x     Model parameters to evaluate.
+ * \param[out] grad  Gradient of the constraint function at \c x.
+ * \param[in]  data  Observed log-likelihood data.
+ *
+ * \return Value of the constraint function at \c x.
+ */
+double lcfit3_cons_theta_b_nlopt(unsigned p, const double* x, double* grad, void* data)
+{
+    const double c = x[0];
+    const double m = x[1];
+    const double theta_b = x[2];
+
+    if (grad) {
+        grad[0] = -1/(c - m) + (c + m)/pow(c - m, 2);
+        grad[1] = -1/(c - m) - (c + m)/pow(c - m, 2);
+        grad[2] = 1.0;
+    }
+
+    return theta_b - (c + m)/(c - m);
+}
+
 int lcfit3n_fit_weighted_nlopt(const size_t n, const double* t, const double* lnl,
                                const double* w, lcfit3_bsm_t* model)
 {
@@ -134,6 +163,7 @@ int lcfit3n_fit_weighted_nlopt(const size_t n, const double* t, const double* ln
     nlopt_set_upper_bounds(opt, upper_bounds);
 
     nlopt_add_inequality_constraint(opt, lcfit3_cons_cm_nlopt, &data, 0.0);
+    nlopt_add_inequality_constraint(opt, lcfit3_cons_theta_b_nlopt, &data, 0.0);
 
     nlopt_set_xtol_rel(opt, sqrt(DBL_EPSILON));
     nlopt_set_maxeval(opt, MAX_ITERATIONS);
